d8c5eda441f2f44099ac79dd9b9c5f2dc05ac367
Daniel Bagnell <daniel.p.bagnell@gmail.com>
2011-03-01 15:14:05 -0500

    Add doc tags to Diagnostic methods and create a stub for a Condition class.

---
+++ Code/exp-design/experimentmanager.hpp
@@ -0,0 +1,37 @@
+#ifndef EXPERIMENTMANAGER_H
+#define EXPERIMENTMANAGER_H
+
+#include <QMainWindow>
+#include <vector>
+using std::vector;
+
+#include "moduleeditor.hpp"
+#include "clusterview.hpp"
+
+namespace Ui {
+    class ExperimentManager;
+}
+
+class ExperimentManager : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit ExperimentManager(QWidget *parent = 0);
+    ~ExperimentManager();
+
+private:
+    Ui::ExperimentManager *ui;
+    ModuleEditor* editor;
+    ClusterView* clusterViewer;
+    void openNewManager();
+
+    vector<ExperimentManager*> managers;
+
+public slots:
+    void on_actionEdit_Modules_triggered();
+    void on_actionOpen_Experiment_triggered();
+    void on_actionRun_Experiment_triggered();
+    void on_actionSave_Experiment_triggered();
+};
+
+#endif // EXPERIMENTMANAGER_H
