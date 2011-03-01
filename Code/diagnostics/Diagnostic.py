04c22fe4f14c472a9adcb2b345689dcb8f7bb3d7
Daniel Bagnell <daniel.p.bagnell@gmail.com>
2011-03-01 15:14:05 -0500

    Add doc tags to Diagnostic methods and create a stub for a Condition class.

--- Code/diagnostics/Diagnostic.py
+++ Code/diagnostics/Diagnostic.py
@@ -4,7 +4,7 @@
 #	Abstract Diagnostic class. Objects of subclasses are added to the an
 #	Experiment object where it is updated.
 #
-# 3/1 - Dan Bagnell - created Diagnostic class
+# 3/1 - Dan Bagnell - created Diagnostic class.
 #
 # Team GPUnit - Senior Design 2011
 #
@@ -13,34 +13,43 @@ class Diagnostic :
 	'''Abstract Diagnostic class. Objects of subclasses are added to the an
 	Experiment object where it is updated.'''
 
-	def __init__(self, name) :
-		self.name = name
+	def __init__(self) :
+		self.name = "Abstract Diagnostic base class"
 		self.conditions = []
 
 	def update(particles) :
+		'''This function needs to be overridden by subclasses'''
 		# update the diagnostic
 		# this method is supposed to be implemented by subclasses
-		# inheritance in python?
 		return False
 
 	def name() :
+		'''Gets the name of the Diagnostic.'''
 		return self.name
 
 	def shouldUpdate(state) :
+		'''Returns a boolean indicating whether this diagnostic should
+		be updated given the current experiment state.'''
 		bUpdate = True
 		for condition in self.conditions :
 			bUpdate = bUpdate and condition.shouldUpdate(state)
 		return bUpdate
 
 	def addCondition(condition) :
+		'''Add a condition.'''
 		self.conditions.append(condition)
 
 	def addConditions(conditions) :
+		'''Add a list of conditions.'''
 		self.conditions.extend(conditions)
 
 	def removeCondition(condition) :
+		'''Remove a condition.'''
 		self.conditions.remove(condition)
 	
 	def toXml() :
+		'''Get a xml string representation.'''
 		# TODO: implement
 		return ""
+
+
