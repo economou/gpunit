62615743b8a0c6a9044abb429fa4f14a7d39330b
Gabe Schwartz <gbs25@drexel.edu>
2011-04-17 16:18:57 -0400

    Added some treeview signals/slots for module toolbox.

--- Code/exp_design/modulestoolbox.py
+++ Code/exp_design/modulestoolbox.py
@@ -1,5 +1,7 @@
+from PyQt4.QtCore import pyqtSlot
 from PyQt4.QtGui import QWidget
 
+from exp_management.PlummerModel import PlummerModel
 from gui.ui_modulestoolbox import Ui_ModulesToolBox
 
 class ModulesToolbox(QWidget):
@@ -8,3 +10,17 @@ class ModulesToolbox(QWidget):
 
         self.ui = Ui_ModulesToolBox()
         self.ui.setupUi(self)
+
+    @pyqtSlot()
+    def test(self, item, i):
+        print "item:", item.text()
+        if i is not None:
+            print i.text()
+        print
+
+        item.addChild(PlummerModel(32))
+
+    @pyqtSlot()
+    def addButtonClicked(self):
+        pass
+        #self.emit(itemAdded, self.ui.
