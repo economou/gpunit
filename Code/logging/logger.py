4fe52b22f79c71e8107c48f622a833dbc61e3886
Gabe Schwartz <gbs25@drexel.edu>
2011-04-17 16:17:54 -0400

    Replaced tabs with four spaces. Added initial conditions base class for GUI interop.

---
+++ Code/logging/logger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#
+# Logger.py
+# Rajkumar Jayachandran
+#
+# Team GPUnit - Senior Design 2011
+
+class Experiment :
+	
+def __init__(self, name) :
+values
+		
+	self.loggers = []
+for logger in self.loggers :
+			XMLString += tabstop*depth
+			XMLString += logger.toXml()
+			XMLString += '\n'
+def addLogger(logger) :
+		
+		self.loggers.append(logger)
+def removeLogger(logger) :
+		
+		self.loggers.remove(logger)
+XMLString += '</experiment>'
+	 
+		outFile = open(fileName, 'w')
+		outFile.write(XMLString)
+
