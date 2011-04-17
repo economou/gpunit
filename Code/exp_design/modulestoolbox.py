906cf62d4dbd0a1893efd8e047658ffde90658a4
Gabe Schwartz <gbs25@drexel.edu>
2011-04-17 18:04:25 -0400

    Moved all initial condition classes to one python module.

--- Code/exp_design/modulestoolbox.py
+++ Code/exp_design/modulestoolbox.py
@@ -1,8 +1,13 @@
 from PyQt4.QtCore import pyqtSlot, SIGNAL
 from PyQt4.QtGui import QWidget
 
-from exp_management.PlummerModel import PlummerModel
+from exp_management.initialconditions import *
+
 from gui.ui_modulestoolbox import Ui_ModulesToolBox
+
+initialConditions = {
+        "Plummer" : PlummerModel
+        }
 
 class ModulesToolbox(QWidget):
     def __init__(self, parent = None):
@@ -10,12 +15,14 @@ class ModulesToolbox(QWidget):
 
         self.ui = Ui_ModulesToolBox()
         self.ui.setupUi(self)
-        self.ui.initCondList.addItem(PlummerModel(32))
+
+        for initCond in initialConditions.values():
+            self.ui.initCondList.addItem(initCond())
 
     @pyqtSlot()
     def addModule(self):
-        self.emit(SIGNAL("moduleAdded"), self.ui.moduleList.currentItem())
+        self.emit(SIGNAL("moduleAdded(PyQt_PyObject)"), self.ui.moduleList.currentItem())
 
     @pyqtSlot()
     def addInitCond(self):
-        self.emit(SIGNAL("initCondAdded"), self.ui.initCondList.currentItem())
+        self.emit(SIGNAL("initCondAdded(PyQt_PyObject)"), self.ui.initCondList.currentItem())
