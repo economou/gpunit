4b9f15050cfa4efe5d9b744f93cdc525f6b9f18f
timmcjilton <timmcjilton@localhost>
2011-04-19 15:50:51 -0400

    Added Energy Grapher that uses some pylab stuff

---
+++ Code/diagnostics/builtin/gldiagnostic.py
@@ -0,0 +1,113 @@
+from time import sleep
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+from PyQt4.QtOpenGL import QGL, QGLFormat, QGLWidget
+from PyQt4.QtCore import QSize
+
+from amuse.support.data.core import Particle, Particles
+
+from diagnostics.Diagnostic import Diagnostic
+
+from PyQt4.QtCore import QCoreApplication
+
+class OpenGLDiagnostic(Diagnostic):
+    def __init__(self, name = "OpenGLDiagnostic", dim = (512, 512)):
+        Diagnostic.__init__(self, name)
+
+        self.width, self.height = dim
+        self.window = GLDiagnosticWindow(self.width, self.height)
+
+    def __reduce__(self):
+        newDict = self.__dict__.copy()
+        del newDict["window"]
+
+        return (OpenGLDiagnostic, (self.name, ), newDict)
+
+    def setSize(self, dim):
+        self.width, self.height = dim
+        self.window.setSize(dim)
+
+    def update(self, time, particles):
+        self.window.particles = particles
+
+        if not self.window.isVisible():
+            self.window.show()
+
+        self.window.update()
+        self.window.paintGL()
+
+        # TODO: THIS IS REALLY BAD.
+        QCoreApplication.processEvents()
+        return True
+
+    def shouldUpdate(self, time, particles):
+        return True
+
+class GLDiagnosticWindow(QGLWidget):
+    def __init__(self, width, height, parent = None):
+        QGLWidget.__init__(self, QGLFormat(QGL.DepthBuffer | QGL.DoubleBuffer), parent)
+
+        self.width_ = width
+        self.height_ = height
+
+        self.particles = Particles(0)
+
+    def initializeGL(self):
+        glClearColor(0,0,0,1.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glPointSize(2.0)
+
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        #glOrtho(-2, 2, -2, 2, -2, 2)
+        gluPerspective(45.0, 1.0, 0.5, 200);
+        glMatrixMode(GL_MODELVIEW)
+        gluLookAt(5, 5, 5,
+                0, 0, 0,
+                0, 0, 1);
+
+    def setSize(self, dim):
+        self.width_, self.height_ = dim
+        self.resizeGL(self.width_, self.height_)
+
+    def resizeGL(self, width, height):
+        self.resize(self.width_, self.height_)
+
+    def maximumSizeHint(self):
+        return QSize(self.width_, self.height_);
+
+    def minimumSizeHint(self):
+        return QSize(self.width_, self.height_);
+
+    def sizeHint(self):
+        return QSize(self.width_, self.height_);
+
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Draw some axes.
+        glBegin(GL_LINES)
+        glColor3f(1,0,0)
+        glVertex3f(0,0,0)
+        glVertex3f(1,0,0)
+
+        glColor3f(0,1,0)
+        glVertex3f(0,0,0)
+        glVertex3f(0,1,0)
+
+        glColor3f(0,0,1)
+        glVertex3f(0,0,0)
+        glVertex3f(0,0,1)
+        glEnd()
+
+        # Draw the points.
+        # TOOD: THIS IS SLOW! Use A VBO or vertex array.
+        glColor3f(1,1,1)
+
+        glBegin(GL_POINTS)
+        for particle in self.particles:
+            pos = particle.position.number
+            glVertex3f(pos[0], pos[1], pos[2])
+        glEnd()
---
+++ Code/exp_design/clusterview.py
@@ -0,0 +1,30 @@
+from PyQt4.QtGui import QMainWindow
+from gui.clusterview_ui import Ui_ClusterView
+
+MAX_NODE_COLS = 3
+"""Maximum number of columns to display in the node grid view."""
+
+class ClusterView(QMainWindow):
+    def __init__(self, parent = None):
+        QMainWindow.__init__(self, parent)
+
+        self.ui = Ui_ClusterView()
+        self.ui.setupUi(self)
+
+        self.nodes = []
+        """List of nodes monitored by this ClusterView window."""
+
+    def addNode(self, node):
+        if node not in self.nodes:
+            row = len(self.nodes) // MAX_NODE_COLS
+            col = len(self.nodes) % MAX_NODE_COLS
+
+            self.nodes.append(node)
+            self.ui.gridLayout.addWidget(node, row, col)
+            self.update()
+
+    def removeNode(self, node):
+        if node in self.nodes:
+            self.nodes.remove(node)
+            self.ui.gridLayout.removeWidget(node)
+            self.update()
