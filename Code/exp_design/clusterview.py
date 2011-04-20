abdf1023dc444f324577e7b4bddef9c8627c85e5
timmcjilton <timmcjilton@localhost>
2011-04-20 18:03:35 -0400

    Changed Diagnostic Formatting to use modules rather than particles

---
+++ Code/exp_design/clusterview.py
@@ -0,0 +1,37 @@
+from PyQt4.QtCore import pyqtSlot
+from PyQt4.QtGui import QMainWindow
+from gui.clusterview_ui import Ui_ClusterView
+
+MAX_NODE_COLS = 3
+"""Maximum number of columns to display in the node grid view."""
+
+class ClusterView(QMainWindow):
+    def __init__(self, parent = None):
+        QMainWindow.__init__(self, parent)
+
+        self.ui = Ui_ClusterView()
+        self.ui.setupUi(self)
+
+        self.nodes = []
+        """List of nodes monitored by this ClusterView window."""
+
+    def resetUI(self):
+        for node in self.nodes: self.removeNode(node)
+        self.nodes = []
+
+    @pyqtSlot()
+    def addNode(self, node):
+        if node not in self.nodes:
+            row = len(self.nodes) // MAX_NODE_COLS
+            col = len(self.nodes) % MAX_NODE_COLS
+
+            self.nodes.append(node)
+            self.ui.gridLayout.addWidget(node, row, col)
+            self.update()
+
+    @pyqtSlot()
+    def removeNode(self, node):
+        if node in self.nodes:
+            self.nodes.remove(node)
+            self.ui.gridLayout.removeWidget(node)
+            self.update()
---
+++ Code/exp_design/diagnosticstoolbox.py
@@ -0,0 +1,39 @@
+from PyQt4.QtCore import pyqtSlot, SIGNAL
+from PyQt4.QtGui import QWidget
+
+from diagnostics.builtin.gldiagnostic import *
+from diagnostics.builtin.energydiagnostic import *
+from diagnostics.builtin.energygrapher import EnergyGrapher
+
+from gui.diagnosticstoolbox_ui import Ui_DiagnosticsToolBox
+
+diagnostics = (
+        OpenGLDiagnostic,
+        EnergyDiagnostic,
+        EnergyGrapher,
+        )
+
+class DiagnosticsToolbox(QWidget):
+    def __init__(self, parent = None):
+        QWidget.__init__(self, parent)
+
+        self.ui = Ui_DiagnosticsToolBox()
+        self.ui.setupUi(self)
+        self.resetUi()
+
+    def resetUi(self):
+        self.ui.diagnosticList.clear()
+        self.ui.loggerList.clear()
+
+        for diag in diagnostics:
+            self.ui.diagnosticList.addItem(diag())
+
+    @pyqtSlot()
+    def addDiagnostic(self):
+        diagnostic = self.ui.diagnosticList.takeItem(self.ui.diagnosticList.currentRow())
+        self.emit(SIGNAL("diagnosticAdded(PyQt_PyObject)"), diagnostic)
+
+    @pyqtSlot()
+    def addLogger(self):
+        logger = self.ui.loggerList.takeItem(self.ui.loggerList.currentRow())
+        self.emit(SIGNAL("loggerAdded(PyQt_PyObject)"), initCond)
