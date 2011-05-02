a84f80e437e3c0781ce508a1ba00be7702585f8c
Gabe Schwartz <gbs25@drexel.edu>
2011-05-02 01:40:12 -0400

    Fixed up running/saving so they setup paths first.

--- Code/exp_management/persistence.py
+++ Code/exp_management/persistence.py
@@ -3,18 +3,17 @@ import cPickle
 from experiment import Experiment
 from exp_gen.CLT import run_experiment
 
-def tryCreateDirs(directory):
+def tryCreateDirs(path):
     """Attempts to create a hierarchy of directories, doing nothing if they
     exist."""
 
-    path = os.path.dirname(directory)
     if not os.path.exists(path):
         os.makedirs(path)
 
 class ExperimentStorage:
 
     @staticmethod
-    def load(location):
+    def load(filename):
         pass
 
     def __init__(self, name):
@@ -22,6 +21,11 @@ class ExperimentStorage:
         self.runs = 0
 
         self.base = Experiment(name)
+
+    def setBase(self, base):
+        self.base = base
+        self.runs = 0
+        self.name = base.name
 
     def save(self, location):
         pass
@@ -31,24 +35,28 @@ class ExperimentStorage:
 
 import os
 
-def FileStorage(ExperimentStorage):
+class FileStorage(ExperimentStorage):
 
     @staticmethod
-    def load(location:
-        storageFile = open(location, 'r')
+    def load(filename):
+        storageFile = open(filename, 'r')
         storage = cPickle.load(storageFile)
         storageFile.close()
         return storage
+
+    def save(self):
+        outFile = open(self.basePath + os.sep + self.name + ".exp", 'w')
+        self.setPaths(self.base)
+
+        cPickle.dump(self, outFile)
+        outFile.close()
 
     def __init__(self, name, basePath = os.getcwd()):
         ExperimentStorage.__init__(self, name)
 
         self.basePath = basePath
 
-    def run(self):
-        self.runs += 1
-        experiment = self.base.copy()
-
+    def setPaths(self, experiment):
         outputDir = self.basePath + os.sep + str(self.runs)
 
         diagnosticsDir = outputDir + os.sep + "diagnostics"
@@ -65,6 +73,7 @@ def FileStorage(ExperimentStorage):
 
         for init in experiment.initialConditions:
             experiment.initialConditionPaths[init] = objectsDir + os.sep + init.name + ".init"
+            init.setStoragePath(objectsDir + os.sep)
 
         for diag in experiment.diagnostics:
             experiment.diagnosticPaths[diag] = objectsDir + os.sep + diag.name + ".diag"
@@ -76,6 +85,11 @@ def FileStorage(ExperimentStorage):
             if logger.needsFile():
                 logger.setupFile(loggingDir + os.sep + logger.name + ".out")
 
+    def run(self):
+        self.runs += 1
+        experiment = self.base.copy()
+
+        self.setPaths(experiment)
         run_experiment(experiment)
 
         for diagnostic in experiment.diagnostics:
