8a6df1379966822416eaeda0e92ea3c553988bbc
timmcjilton <timmcjilton@localhost>
2011-05-17 16:05:21 -0400

    Finished Initialization tests. Now onto the CLT run experiment tests

--- Code/pyunit_tests/clt_test.py
+++ Code/pyunit_tests/clt_test.py
@@ -13,6 +13,7 @@ from exp_management.experiment import Experiment
 from amuse.support.data.core import Particles
 from amuse.support.units import nbody_system
 from amuse.support.units import units
+
 #from amuse.support.data.particles import Particles
 
 from diagnostics.builtin.energydiagnostic import EnergyDiagnostic
@@ -47,18 +48,25 @@ class CLTTestCase(unittest.TestCase):
         my_experiment.modules.append(Module.fromFile("../exp_management/Modules_XML/hermite0.xml"))
 
         self.my_experiment = my_experiment #cPickle.load(open("test/test.exp",'r'))
-    def test_initialization(self):
+    def test_initialization_modules(self):
         self.setup()
         modules,particles,convert_nbody = initialization(self.my_experiment)
         self.assertTrue(len(modules) > 0, "Module list is too small")
         self.assertTrue(isinstance(modules[0],Hermite),"Module is suppose to be Hermite type Module")
         self.assertTrue(None == modules[0].evolve_model(1|units.s), "Evolve model returns bad stuff")
+    def test_initialization_particles(self):
+        self.setup()
+        modules,particles,convert_nbody = initialization(self.my_experiment)
         self.assertTrue(isinstance(particles,Particles),"Particles are suppose to be Particles type class")
-#        self.assertTrue(isinstance(convert_nbody
-        print dir(self)
-       
-       # self.assertTrue(
-        
+        self.assertTrue(len(particles) == 2,"Their is a wrong number of Particles")
+    def test_initialization_conversion(self):
+        self.setup()
+        modules,particles,convert_nbody = initialization(self.my_experiment)
+        self.assertTrue(isinstance(convert_nbody,nbody_system.nbody_to_si),"Convert Nbody is Wrong Class Type")
+        self.assertTrue(abs(convert_nbody.to_si(1|nbody_system.length).value_in(units.parsec) - 1.0) < 1e-7,
+            "Testing that the conversion Converts length correct")
+        self.assertTrue(abs(convert_nbody.to_si(1|nbody_system.mass).value_in(units.MSun) - 1000.0) < 1e-7,
+            "Testing that the conversion Converts mass correct")
     def test_run_experiment(self):
         pass
 if __name__=="__main__":
